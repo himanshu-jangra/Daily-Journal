41f1fe024f99312c64a88c96a820dc1654ace5ae
Himanshu Jangra <himanshuhack@hotmail.com>
2026-03-30 00:14:43 +0530

    Updated the Logo

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
+      includeAssets: ['favicon.png', 'apple-touch-icon.png'],
       manifest: {
         name: 'Daily Journal',
         short_name: 'Journal',
